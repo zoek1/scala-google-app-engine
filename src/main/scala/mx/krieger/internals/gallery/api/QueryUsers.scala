1216d3ecf3c0a370ea510865752a91e456278208
Miguel Angel Gordian <os.aioria@gmail.com>
2016-06-09 18:04:41 -0500

    Declara statement para hacer queries del modelo

--- src/main/scala/mx/krieger/internals/gallery/api/QueryUsers.scala
+++ src/main/scala/mx/krieger/internals/gallery/api/QueryUsers.scala
@@ -1,11 +1,8 @@
 package mx.krieger.internals.gallery.api
 
 import java.util
-import scala.collection.JavaConversions._
 
 import com.google.api.server.spi.config.{Api, ApiMethod, AuthLevel}
-import com.googlecode.objectify.ObjectifyService
-import com.googlecode.objectify.ObjectifyService._
 import mx.krieger.internals.gallery.models.RegisterUserProfile
 
 @Api(name =  "queryService",
@@ -13,25 +10,19 @@ import mx.krieger.internals.gallery.models.RegisterUserProfile
   description = "Services for querying users",
   authLevel = AuthLevel.NONE)
 class QueryUsers {
+
   @ApiMethod(name = "sortByLastName",
     path="sortByParent",
     httpMethod = "GET",
     authLevel = AuthLevel.NONE)
-  def sortByParent(): util.List[RegisterUserProfile] = {
+  def sortByParent(): util.List[RegisterUserProfile] =
+    RegisterUserProfile.fact { query =>
+      val list = query.order("lastname").list
 
-    val reg = new RegisterUserProfile()
-    val l = new util.ArrayList[RegisterUserProfile]()
-    ObjectifyService.register(reg.getClass)
-
-    val list = ofy()
-      .load
-      .`type`( new RegisterUserProfile().getClass )
-      .order("lastname")
-      .list
-
-    list match {
-      case list: util.List[RegisterUserProfile @unchecked] => list
-      case _ => new util.ArrayList[RegisterUserProfile]()
+      list match {
+        case list: util.List[RegisterUserProfile @unchecked] => list
+        case _ => new util.ArrayList[RegisterUserProfile]()
+      }
     }
-  }
+
 }
--- src/main/scala/mx/krieger/internals/gallery/models/RegisterUserProfile.scala
+++ src/main/scala/mx/krieger/internals/gallery/models/RegisterUserProfile.scala
@@ -1,8 +1,11 @@
 package mx.krieger.internals.gallery.models
 
+import com.googlecode.objectify.ObjectifyService
+import com.googlecode.objectify.ObjectifyService._
 import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 import com.googlecode.objectify.annotation.Index
+import com.googlecode.objectify.cmd.LoadType
 import mx.krieger.internals.gallery.wrappers.RegisterRequest
 
 import scala.beans.BeanProperty
@@ -25,6 +28,7 @@ class RegisterUserProfile {
   @BeanProperty
   var urlPhoto: String = URL_PROFILE
 
+
   def this(registerRequest: RegisterRequest) {
     this()
     this.firstname = registerRequest.getFirstname
@@ -37,3 +41,17 @@ class RegisterUserProfile {
 
 }
 
+object RegisterUserProfile {
+  def fact[T](element: (LoadType[RegisterUserProfile]) => T): T = {
+    ObjectifyService.register(new RegisterUserProfile().getClass)
+
+    val query = ofy()
+      .load
+      .`type`( new RegisterUserProfile().getClass )
+
+    query match {
+      case query: LoadType[RegisterUserProfile @unchecked] =>
+        element(query)
+    }
+  }
+}
